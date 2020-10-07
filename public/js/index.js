89bfe924eda29d1db852b3b99eb57e06ba5ccac8
yl3030 <yl301114@gmail.com>
2020-10-08 00:01:35 +0800

    ccc

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+$("*").each(function(){
+    $(this).click(function(){
+        var target = $(this).attr("data-goto-target");
+        if(target!=undefined) {
+            var dur = $(this).attr("data-goto-duration");
+            var top = $(target).offset().top;
+            $("html,body").animate({scrollTop:top},parseInt(dur));
+        }
+    })
+});
+$(window).on("mousewheel",function(){
+    $("*").stop();
+});
+
+$(".classified").children("button").click(function(){
+    $(".classified").children("button").removeClass("active");
+    $(this).addClass("active");
+    $(".port-cont").parents(".port-box").fadeOut(500);
+    if($(this).hasClass("btn-all")){
+        $(".port-cont").parents(".port-box").fadeIn(500);
+    }else if($(this).hasClass("btn-web")){
+        $(".port-cont.web").parents(".port-box").fadeIn(500);
+    }else if($(this).hasClass("btn-app")){
+        $(".port-cont.app").parents(".port-box").fadeIn(500);
+    }else if($(this).hasClass("btn-graphic")){
+        $(".port-cont.graphic").parents(".port-box").fadeIn(500);
+    }
+})
